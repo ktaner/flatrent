96a6027a9e7429f63e5a0930e30ff49bc64ff77b
ktaner <kt@honeypot.io>
2017-03-31 19:48:27 +0200

    add bootstrap+new properties view

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,30 @@
 class PropertiesController < ApplicationController
+
+
+  def new
+    @property = Property.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+    @property = Property.find(params[:id])
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+
+
+
 end
